aa8089a423c4c6e215c8704d9bc31139d7f12ba2
GPaSalini <43826220+GPaSalini@users.noreply.github.com>
2018-10-07 23:22:24 -0300

    libreria
    //Estructura de imagen que utiliza el motor
    sprite_graphics.h

---
+++ sprite_graphics.h
@@ -0,0 +1,21 @@
+/*
+ * sprite_graphics.h
+ *
+ *  Created on: 04-06-2018
+ *      Author: Giordano
+ */
+
+#ifndef INC_SPRITE_GRAPHICS_H_
+#define INC_SPRITE_GRAPHICS_H_
+
+#include <stdint.h>
+
+//Estructura para una imagen
+typedef struct _Sprite_Image
+	{
+	uint16_t width;
+	uint16_t height;
+	const uint32_t *data;
+} Sprite_Image;
+
+#endif /* INC_SPRITE_GRAPHICS_H_ */
